3ec45d94d7222f21a8ee4e2a15c2b8f46d06195c
matinict <matinict@gmail.com>
2018-10-02 10:21:59 +0600

    code push

---
+++ Block/Seller.php
@@ -0,0 +1,10 @@
+<?php
+namespace Sslwireless\Seller\Block;
+
+class Seller extends \Magento\Framework\View\Element\Template
+{
+    public function getSellerTxt()
+    {
+        return 'My Customer Registration Form Here...!';
+    }
+}
